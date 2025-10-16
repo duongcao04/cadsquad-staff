794baa55f942f474c2e709c86c21eb5c738758d9
Duong Cao <caohaiduong04@gmail.com>
2025-10-16 21:44:20 +0700

    Upp

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[] = []) => {
