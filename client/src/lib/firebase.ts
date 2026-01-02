47b7573294c6a015d006e54b897bcdbb1266edfe
Duong Cao <caohaiduong04@gmail.com>
2026-01-02 18:30:30 +0700

    Relase v.000001

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -1,7 +1,20 @@
-import { initializeApp } from 'firebase/app'
-import { getMessaging } from 'firebase/messaging'
+import { FirebaseApp, initializeApp } from 'firebase/app'
+import { getMessaging, isSupported } from 'firebase/messaging'
 import { firebaseConfig } from './config'
 
 const app = initializeApp(firebaseConfig)
+export const getMessagingSafe = async (app: FirebaseApp) => {
+    try {
+        const supported = await isSupported()
+        if (supported) {
+            return getMessaging(app)
+        }
+        console.warn('FCM không được hỗ trợ trên trình duyệt này.')
+        return null
+    } catch (err) {
+        console.error('Lỗi khởi tạo Messaging:', err)
+        return null
+    }
+}
 export const messaging =
-    typeof window !== 'undefined' ? getMessaging(app) : null
+    typeof window !== 'undefined' ? getMessagingSafe(app) : null
