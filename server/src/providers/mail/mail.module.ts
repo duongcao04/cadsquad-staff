d4c32ee9ac944dbb6baa942e1157851886ee5b2e
Duong Cao <caohaiduong04@gmail.com>
2026-01-03 19:36:22 +0700

    fix mail

--- server/src/providers/mail/mail.module.ts
+++ server/src/providers/mail/mail.module.ts
@@ -16,7 +16,7 @@ import { MailService } from './mail.service'
                     },
                 },
                 template: {
-                    dir: join(process.cwd(), 'src/providers/mail/templates'),
+                    dir: join(process.cwd(), 'src/common/templates'),
                     adapter: new HandlebarsAdapter({
                         // Định nghĩa các logic so sánh cho template .hbs
                         eq: (a: any, b: any) => a === b,
