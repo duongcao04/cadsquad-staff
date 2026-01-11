7ac95937cd133fb11e9b4927904a34d66a914d34
Duong Cao <caohaiduong04@gmail.com>
2026-01-12 00:44:40 +0700

    Fix email

--- server/src/providers/mail/mail.module.ts
+++ server/src/providers/mail/mail.module.ts
@@ -5,31 +5,34 @@ import { join } from 'path'
 import { MailService } from './mail.service'
 import { EmailController } from './email.controller'
 @Module({
-    imports: [
-        MailerModule.forRootAsync({
-            useFactory: () => ({
-                transport: {
-                    host: process.env.MAIL_HOST,
-                    auth: {
-                        user: process.env.MAIL_USER,
-                        pass: process.env.MAIL_PASS,
-                    },
-                },
-                template: {
-                    dir: join(__dirname, 'templates'),
-                    adapter: new HandlebarsAdapter({
-                        // Định nghĩa các logic so sánh cho template .hbs
-                        eq: (a: any, b: any) => a === b,
-                    }),
-                    options: {
-                        strict: true,
-                    },
-                },
-            }),
-        }),
-    ],
-    controllers: [EmailController],
-    providers: [MailService],
-    exports: [MailService],
+	imports: [
+		MailerModule.forRootAsync({
+			useFactory: () => ({
+				transport: {
+					host: process.env.MAIL_HOST,
+					auth: {
+						user: process.env.MAIL_USER,
+						pass: process.env.MAIL_PASS,
+					},
+				},
+				defaults: {
+					from: `"No Reply" <${process.env.MAIL_FROM}>`, // Good practice to set a default sender
+				},
+				template: {
+					dir: join(process.cwd(), 'templates'),
+					adapter: new HandlebarsAdapter({
+						// Định nghĩa các logic so sánh cho template .hbs
+						eq: (a: any, b: any) => a === b,
+					}),
+					options: {
+						strict: true,
+					},
+				},
+			}),
+		}),
+	],
+	controllers: [EmailController],
+	providers: [MailService],
+	exports: [MailService],
 })
 export class MailModule {}
